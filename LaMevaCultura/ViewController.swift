9ffdc49e72111734f103df41c7e24e0aaab0c58c
QueraltSM <queraltsosa@hotmail.com>
2017-06-13 12:57:11 +0100

    Create ViewController.swift

--- LaMevaCultura/ViewController.swift
+++ LaMevaCultura/ViewController.swift
@@ -7,19 +7,31 @@
 //
 
 import UIKit
-
 class ViewController: UIViewController {
+    var counter = 0
+    var timer = Timer()
+    
+    func timerAction() {
+        counter += 1
+        if (counter == 8) {
+            if let storyboard = self.storyboard {
+                let nextViewController = storyboard.instantiateViewController(withIdentifier: "Menu")
+                self.present(nextViewController, animated:true, completion:nil)
+            }
+        }
+    }
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        timer.invalidate()
+            timer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
     }
+    
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
